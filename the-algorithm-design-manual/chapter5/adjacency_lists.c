090adc9038c47104acaa2fa9228c6c68cccb14ad
an9wer <an9wer@gmail.com>
2017-09-02 17:29:55 +0800

    adjacency lists

---
+++ the-algorithm-design-manual/chapter5/adjacency_lists.c
@@ -0,0 +1,109 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAXV 1000
+
+typedef enum {FALSE, TRUE} bool;
+
+typedef struct edgenode{
+    int y;                  // adjacency info
+    int weight;             // edge weight, if any
+    struct edgenode *next;  // next edge in list
+} edgenode;
+
+typedef struct {
+    edgenode *edges[MAXV+1];    // adjacency info
+    int degree[MAXV+1];         // outdegree of each vertex
+    int nvertices;              // number of vertices in graph
+    int nedges;                 // number of edges in graph
+    bool directed;              // is the graph directed
+} graph;
+
+void initialize_graph(graph *, bool);
+void read_graph(graph *, bool);
+void insert_edge(graph *, int, int, bool);
+void print_graph(graph *);
+
+int main(void) {
+    graph *pg = malloc(sizeof(graph));
+    initialize_graph(pg, FALSE);
+    read_graph(pg, FALSE);
+    print_graph(pg);
+    return 0;
+}
+
+void initialize_graph(graph *g, bool directed)
+{
+    int i;      // counter
+    
+    g -> nvertices = 0;
+    g -> nedges = 0;
+    g -> directed = directed;
+
+    for (i = 1; i <= MAXV; i++)
+        g -> degree[i] = 0;
+    for (i = 1; i <= MAXV; i++)
+        g -> edges[i] = NULL;
+}
+
+void read_graph(graph *g, bool directed)
+{
+    int i;      // counter
+    int m;      // number of edges
+    int x, y;   // vertices in edge (x, y)
+
+    initialize_graph(g, directed);
+
+    printf("enter nvertices and nedges: ");
+    scanf("%d%d", &(g->nvertices), &m);
+    
+    for (i = 1; i <= m; i++) {
+        printf("enter vertex x and y: ");
+        scanf("%d%d", &x, &y);
+        insert_edge(g, x, y, directed);
+    }
+}
+
+/*
+ * an edge (x, y), the vertices x and y are said to be 
+ * incident to the edge and adjacent to each other.
+ */
+void insert_edge(graph *g, int x, int y, bool directed)
+{
+    /*
+     * param x: the vertex x of edge(x, y)
+     * param y: the vertex y of edge(x, y)
+     */
+
+    edgenode *p;    // temporary pointer
+    
+    p = malloc(sizeof(edgenode));
+
+    p -> weight = NULL;
+    p -> y = y;
+    p -> next = g -> edges[x];
+
+    g -> edges[x] = p;
+    g -> degree[x] ++;
+
+    if (directed == FALSE)
+        insert_edge(g, y, x, TRUE);
+    else
+        g -> nedges ++;
+}
+
+void print_graph(graph *g)
+{
+    int i;          // counter
+    edgenode *p;    // temporary pointer
+
+    for (i = 1; i <= g->nvertices; i++) {
+        printf("%d: ", i);
+        p = g -> edges[i];
+        while (p != NULL) {
+            printf(" %d", p -> y);
+            p = p->next;
+        }
+        printf("\n");
+    }
+}
